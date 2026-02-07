9fe1dc1a1bb05fb2337a0e7c9696fce38109ed97
Iretomiwa Lawal <iretomiwaemma@gmail.com>
2026-02-07 15:25:08 +0100

    update

--- src/Components/UI/CareersModal.jsx
+++ src/Components/UI/CareersModal.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CareersModal = ({ isOpen, onClose }) => {
   if (!isOpen) return null;
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center ">
+    <div className="fixed inset-0 z-50 flex items-center justify-center m-4 ">
       <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md relative">
         <button
           className="absolute top-2 right-2 text-gray-500 hover:text-health-pink text-xl"
